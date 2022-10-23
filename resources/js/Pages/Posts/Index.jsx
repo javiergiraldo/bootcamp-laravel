6b7a92237ea1fff40afcb9991c530bd7e7faf4d2
Javier Giraldo <pac8314@gmail.com>
2022-10-23 00:44:40 -0500

    Pages

---
+++ resources/js/Pages/Posts/Index.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
+import InputError from '@/Components/InputError'
+import PrimaryButton from '@/Components/PrimaryButton'
+import { useForm } from '@inertiajs/inertia-react'
+
+export const Index = ({auth}) => {
+    const { data, setData, post, processing, reset, errors } = useForm({
+        title: '',
+        body: ''
+    })
+
+    const submit = (e) => {
+        e.preventDefault()
+        post(route('posts.store'), {onSuccess: ()=> reset() })
+    }
+
+  return (
+    <AuthenticatedLayout auth={auth}>
+        <head title='posts'/>
+        <div className='max-w-2xl mx-auto p-4 sm:p-6 lg:p-8'>
+            <form onSubmit={submit}>
+                <input 
+                 />
+                 <InputError message={errors.title} className="mt-2"/>
+                 <textarea 
+                 >                    
+                 </textarea>
+                 <InputError message={errors.title} className="mt-2"/>
+                 <PrimaryButton
+                 className='mt-4 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'
+                 disabled={processing}
+                 >
+                    Create
+                 </PrimaryButton>
+            </form>
+        </div>
+    </AuthenticatedLayout>
+  )
+}
+
+export default Index 
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
